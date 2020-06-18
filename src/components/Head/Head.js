0abdc337c247b934b405c5170318a3cc9d685ed2
Jaime Morales <jaime@lastrev.com>
2020-06-18 15:36:26 -0700

    using all values from SEO UI Ext.

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,19 +1,41 @@
 import React from 'react';
-import propTypes from './Head.propTypes';
 import NextHead from 'next/head';
+import propTypes from './Head.propTypes';
 
-function Head({ title, description, keywords }) {
-  const getValue = (obj) => {
-    return obj ? obj.value : '';
-  };
+const getValue = (obj) => {
+  return obj ? obj.value : '';
+};
 
+const getUrl = (obj) => {
+  const val = getValue(obj);
+  return val ? val.url : '';
+};
+
+function Head({
+  title,
+  description,
+  keywords,
+  robots,
+  'og:image': ogImage,
+  'og:title': ogTitle,
+  'og:description': ogDescription,
+  'twitter:image': twitterImage,
+  'twitter:title': twitterTitle,
+  'twitter:description': twitterDescription
+}) {
   return (
     <NextHead>
-      <title data-testid="Head-title" key="title">
-        {getValue(title)}
-      </title>
-      <meta name="description" key="description" content={getValue(description)} data-testid="Head-description" />
-      <meta name="keywords" key="keywords" content={getValue(keywords)} data-testid="Head-keywords" />
+      <title key="title">{getValue(title)}</title>
+
+      <meta name="description" key="description" content={getValue(description)} />
+      <meta name="keywords" key="keywords" content={getValue(keywords)} />
+      <meta name="robots" key="robots" content={getValue(robots)} />
+      <meta property="og:image" content={getUrl(ogImage)} />
+      <meta property="og:title" content={getValue(ogTitle)} />
+      <meta property="og:description" content={getValue(ogDescription)} />
+      <meta name="twitter:image" content={getUrl(twitterImage)} />
+      <meta name="twitter:title" content={getValue(twitterTitle)} />
+      <meta name="twitter:description" content={getValue(twitterDescription)} />
 
       <link rel="icon" href="/favicon.ico" />
       <meta name="contentful_space" content={process.env.CONTENTFUL_SPACE_ID} key="spaceId" />
@@ -27,7 +49,8 @@ Head.propTypes = propTypes;
 Head.defaultProps = {
   title: null,
   description: null,
-  keywords: null
+  keywords: null,
+  robots: null
 };
 
 export default Head;
--- src/components/Head/Head.propTypes.js
+++ src/components/Head/Head.propTypes.js
@@ -1,16 +1,28 @@
 import PropTypes from 'prop-types';
 
+const standardValuePropType = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired
+});
+
+const imageValuePropType = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  value: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  }).isRequired
+});
+
 export default {
-  title: PropTypes.shape({
-    name: PropTypes.string,
-    value: PropTypes.string
-  }),
-  description: PropTypes.shape({
-    name: PropTypes.string,
-    value: PropTypes.string
-  }),
-  keywords: PropTypes.shape({
-    name: PropTypes.string,
-    value: PropTypes.string
-  })
+  'title': standardValuePropType,
+  'description': standardValuePropType,
+  'keywords': standardValuePropType,
+  'robots': standardValuePropType,
+  'og:title': standardValuePropType,
+  'og:image': imageValuePropType,
+  'og:description': standardValuePropType,
+  'twitter:title': standardValuePropType,
+  'twitter:image': imageValuePropType,
+  'twitter:description': standardValuePropType
 };
