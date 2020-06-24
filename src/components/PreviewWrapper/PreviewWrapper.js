b55887a564acb2ff6577a986a7f8e942b795c730
Jaime Morales <jaime@lastrev.com>
2020-06-24 15:53:12 -0700

    preview wrapper

--- src/components/PreviewWrapper/PreviewWrapper.js
+++ src/components/PreviewWrapper/PreviewWrapper.js
@@ -1,33 +1,18 @@
-/* eslint-disable no-console */
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-named-as-default-member */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect, useState } from 'react';
-import lookupComponentByContentType from '../../utils/lookupComponentByContentType';
-import { getFullContentById } from '../../integrations/contentful';
-import propTypes from './PreviewWrapper.propTypes';
-// TODO: this is very coupled to Contentful, but that may just be the case with this component.
-import Adapter from '../../adapters/contentful';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function PreviewWrapper({ id, contentType, urlMap }) {
-  if (!id && !contentType) return null;
-  const transform = Adapter({ urlMap });
-  const [content, setContent] = useState();
-  useEffect(async () => {
-    const myContent = await getFullContentById(contentType, id);
-    setContent(transform(myContent));
-  });
-  if (!content) return null;
-  try {
-    const Component = lookupComponentByContentType(contentType);
-    return <Component {...content} />;
-  } catch (err) {
-    // TODO: replace with logger
-    console.log('Error rendering preview', err);
-    return null;
-  }
+export const PreviewWrapperPropTypes = {
+  componentLookup: PropTypes.func.isRequired,
+  contentTypeId: PropTypes.string.isRequired
+};
+
+function PreviewWrapper({ componentLookup, contentTypeId, ...fields }) {
+  const Component = componentLookup(contentTypeId);
+
+  return <Component {...fields} />;
 }
 
-PreviewWrapper.propTypes = propTypes;
+PreviewWrapper.propTypes = PreviewWrapperPropTypes;
 
 export default PreviewWrapper;
